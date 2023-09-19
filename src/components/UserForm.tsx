252d71eac93609e379045fdc4cae1a3bc349ae21
Manneta Othman <99536079+Manneta-Othman@users.noreply.github.com>
2023-09-19 10:05:09 +0100

    Update UserForm.tsx

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -12,15 +12,15 @@ export function UserForm({data, updateData}:DataProps) {
           <>
             <h1>User Details</h1>
             <div className="fname">
-              <label htmlFor="fname">First name</label>
+              <label htmlFor="fname">First name*</label>
               <input type="text" autoFocus required id="fname" value={data.firstName} onChange={e => updateData({firstName: e.target.value})} />
             </div>
             <div className="lname">
-              <label htmlFor="lname">last name</label>
+              <label htmlFor="lname">last name*</label>
               <input type="text" required id="lname" value={data.lastName} onChange={e => updateData({lastName: e.target.value})} />
             </div>
             <div className="age">
-              <label htmlFor="age">Age</label>
+              <label htmlFor="age">Age*</label>
               <input type="number" required id="age" min={10} max={100} value={data.age} onChange={e => updateData({age: e.target.value})}/>
             </div>
           </>
